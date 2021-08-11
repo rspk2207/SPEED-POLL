81c4006ea2468d9713f813250fa3b4d9c87bf5ad
rspk2207 <83638002+rspk2207@users.noreply.github.com>
2021-08-12 01:04:44 +0530

    Upto Invitation

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(session({
     secret: "Welcome to your workspace",
-    resave: false,
+    resave: true,
     saveUninitialized: false
 }));
 app.use(passport.initialize());
@@ -73,7 +73,9 @@ app.post('/registration',async (req,res) =>{
                     dob: rdob,
                     phno: rphno,
                     mail: rmail,
-                    password: rpassword
+                    password: rpassword,
+                    teams: 0,
+                    invcount: 0
                 });
 
                 await bcrypt.genSalt(10, async (err,salt)=>{
@@ -114,17 +116,147 @@ function loggedIn(req, res, next) {
 }
 app.get('/homepage',loggedIn,(req,res)=>{
    res.render('homepage',{user: req.user});
+
 });
-app.get('/teamcr',(req,res)=>{
+app.get('/homepage/teamcr',loggedIn,(req,res)=>{
     res.render('teamcr',{user: req.user});
 });
-app.post('/homepage',(req,res)=>{
-    console.log(req.body.teamname);
+app.post('/homepage/teamcr',loggedIn, async (req,res,next)=>{
+    let count = req.user.teams;
+    count++;
+await User.findById(req.user.id,(err,user)=>{
+    if(err)
+    {
+        console.log(err);
+    }
+    else
+    {
+        user.teamnames[count-1] = req.body.teamname;
+        (user.teams)++;
+        user.votes[count-1] = 0;
+        user.isAdmin[count-1] = true;
+        user.markModified('teamnames');
+        user.markModified('votes');
+        user.markModified('isAdmin');
+        user.save();
+        req.user = user.toObject();
+    }
+    });
+
+
+    res.redirect('/homepage');
 });
-app.get('/teams',loggedIn,(req,res)=>{
+app.get('/homepage/teams',loggedIn,(req,res)=>{
     res.render('teams',{user: req.user});
 });
-app.get('/about',loggedIn,(req,res)=>{
+app.get('/homepage/teams/:k',loggedIn,(req,res)=>
+{   
+    let pagename = (req.params.k).toUpperCase();
+    let index = req.params.k;
+    for(let i=0;i<req.user.teamnames.length;i++)
+    {
+    if(index === (req.user.teamnames[i]).toUpperCase())
+    {
+        index = i;
+        break;
+    }
+    }
+    res.render('teampage',{user: req.user, index: index,page: pagename})
+});
+app.post('/homepage/teams/:k/invites',loggedIn,async (req,res)=>{
+    let index = req.params.k;
+    console.log(index);
+    console.log(req.body.inv);
+    await User.findOne({username: req.body.inv},(err,doc)=>{
+        if(err)
+        {
+            console.log(err);
+        }
+        else
+        {
+            if(!doc)
+            {
+                console.log("Invalid user");
+                res.redirect('/homepage/teams/'+index);
+            }
+            else
+            {
+               doc.invites[doc.invcount] = index;
+               (doc.invcount)++;
+               doc.markModified('invites');
+               doc.save(); 
+            }
+        }
+    });
+    res.redirect('/homepage/teams/'+index);
+});
+app.get('/homepage/accepted/:i',loggedIn, async (req,res)=>{
+    let count = parseInt(req.params.i);
+    await User.findOne({username: req.user.username},(err,user)=>{
+        if(err)
+        {
+            console.log(err);
+        }
+        else
+        {
+            if(!user)
+            {
+                conole.log('no user');
+                res.redirect('/homepage');
+            }    
+            else
+            {
+                let invname = user.invites[count-1];
+                user.teamnames[user.teams] = user.invites[count-1];
+                user.invites.pull(invname);
+                (user.invcount)--;
+                user.votes[user.teams] = 0;
+                user.isAdmin[user.teams] = false;
+                (user.teams)++; 
+                user.markModified('teamnames');
+                user.markModified('invites');
+                user.save();
+                console.log(user);
+                req.user = user.toObject();
+                console.log(req.user);
+                res.redirect('/homepage');
+            }
+        }
+    });
+});
+app.get('/homepage/rejected/:i',loggedIn,async (req,res)=>{
+    let count = parseInt(req.params.i);
+    await User.findOne({username: req.user.username},(err,user)=>{
+        if(err)
+        {
+            console.log(err);
+        }
+        else
+        {
+            if(!user)
+            {
+                conole.log('no user');
+                res.redirect('/homepage');
+            }    
+            else
+            {
+                let invname = user.invites[count-1];
+                user.invites.pull(invname);
+                (user.invcount)--;
+                user.markModified('invites');
+                user.save();
+                console.log(user);
+                req.user = user.toObject();
+                console.log(req.user);
+                res.redirect('/homepage');
+            }
+        }
+    });
+});
+app.get('/homepage/teams/:k/pollcr',(req,res)=>{
+    res.render('pollcr',{user: req.user,page: req.params.k});
+})
+app.get('/homepage/about',loggedIn,(req,res)=>{
     res.render('about',{user: req.user});
 });
 app.listen(3000, () => {
